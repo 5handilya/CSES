1e7b0a89a048d8d1718b4b0e28ea97939a197b48
kunal <kun.shandilya@gmail.com>
2024-01-07 09:50:47 +0530

    first

---
+++ 1. weird algorithm.py
@@ -0,0 +1,21 @@
+n = int(input())
+printarray=[]
+
+##even div by 2 odd times 3 plus 1
+
+def check(m):
+    printarray.append(m)
+    if (m==1):
+        pass
+    elif (m%2==0):
+        m = int(m/2)
+        check(m)
+    else:
+        m = m*3 + 1
+        check(m)
+    
+check(n)
+ops = ""
+for i in printarray:
+    ops += (" " + str(i))
+print(ops)
