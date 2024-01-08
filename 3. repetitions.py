4bf649cf5574bd555a489278a9af8d50171b97d2
8vakr <f4232@outlook.com>
2024-01-08 13:38:26 +0530

    third problem solved

--- 3. repetitions.py
+++ 3. repetitions.py
@@ -5,7 +5,7 @@ loccount = 1
 while i < len(seq) - 1:
     if seq[i] == seq[i+1]:
         loccount += 1
-    else:
+    else: #new substr
         loccount = 1
     if loccount > maxcount:
         maxcount = loccount
