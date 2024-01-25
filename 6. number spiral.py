55dda1fa9cef40ba6eb001101d11d3e946f53b96
8vakr <f4232gp@outlook.com>
2024-01-26 05:03:22 +0530

    #6 optimized

--- 6. number spiral.py
+++ 6. number spiral.py
@@ -1,6 +1,5 @@
 ############################
 t = int(input())
-import sys
 i = 0
 inputs = []
 while i < t:
@@ -16,14 +15,16 @@ for currinp in inputs:
     #calculate anchor - closest square minus diff
     x = int(currinp[0])
     y = int(currinp[1])
-    mx = max(x,y)
+    mx = y if (y > x) else x
     anchor_val = mx**2 - (mx - 1)
 
     #calculate relative value using odd-even algo
-    anchor_pos = [max(x,y),max(x,y)]
-    if (anchor_pos[0]) % 2 == 0:
-        rel_pos_val = anchor_val + (anchor_pos[0] - y) - (anchor_pos[0] - x)
+    if (mx) % 2 == 0:
+        rel_pos_val = anchor_val + (x - y)
     else:
-        rel_pos_val = anchor_val + (anchor_pos[0] - x) - (anchor_pos[0] - y)
+        rel_pos_val = anchor_val + (y - x)
+
+    #md = (-1)**(mx % 2) #1 even -1 odd
+    #rel_pos_val = anchor_val + md*(x - y)
 
     print(rel_pos_val)
