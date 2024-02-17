3766c0b6b64303b1dccb946835c8f35c9dbe9214
8vakr <155867142+8vakr@users.noreply.github.com>
2024-02-18 00:23:42 +0530

    8 mod

--- 8. two sets.py
+++ 8. two sets.py
@@ -8,10 +8,21 @@ else:
     A = []
     B = []
     for i in range(n, 0, -1):
+        print("I",i)
+
+        # Halfsum will accomodate descending members of set N (1 to N) in A until halfsum < next N member
+        # then, min(A) - halfsum is, of course, in N and thus guaranteed to be in the iteration
+        # thus, exhaustion of halfsum by set A is guaranteed, ie set A and B are correctly computed
+
         if (i <= halfsum):
+            print("HS",halfsum)
             halfsum -= i
+            print("HS-", halfsum)
+            print("A add:", i)
             A.append(i)
         else:
+            print("HS",halfsum)
+            print("B add: ",i)
             B.append(i)
     print(len(A))
     print(*A)
