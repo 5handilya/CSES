d241b044fc0c6b74b156b568440b3728862bc5c7
8vakr <kunalshandilyawork@gmail.com>
2024-02-27 03:58:42 +0530

    11 solved

--- 11-coin-piles.cpp
+++ 11-coin-piles.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-
 using namespace std;
 int main (){
     int t;
     cin >> t;
-    // input a[] and b[]
-    
-    /**
-     * filter1: eliminate easy cases ie a|b <1, a&b=3n, etc 
-     * ??strat 1: calc a:b, figure out remainders (a/b)/(1/2)
-     * filter2: if (a-b) > min(a,b) bc min not enough to compensate diff even with -1s 
-     * key values: (a - b), min(a,b) % 2,  
-    */
-    
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        if (a < b) swap(a , b); //learned i can just do this instead of using minmax
+        if (b == 0 && a != 0){
+            cout << "NO" << endl; //filtering 0,0 cases
+        }
+        else if (a > 2*b){
+            cout << "NO" << endl; //there is no hope, even with only -1 hits to b
+        }
+        else {
+            a %= 3; //-3 to each after alternating moves ..
+            b %= 3; //.. remaining |a-b| is the key metric
+            if (a < b) swap (a, b);
+            if((a == 0 && b == 0) || (a == 2 && b == 1)){
+                cout << "YES" << endl;
+            }
+            else{
+                cout << "NO" << endl;
+            }
+        }
+    } 
     return 0;
 }
