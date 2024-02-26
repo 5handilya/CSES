ff444e8ce8a28c2d7a866aae8f9313e8bb7dba3a
8vakr <kunalshandilyawork@gmail.com>
2024-02-27 02:58:50 +0530

    progress with 11

--- 11-coin-piles.cpp
+++ 11-coin-piles.cpp
@@ -10,7 +10,7 @@ int main (){
      * filter1: eliminate easy cases ie a|b <1, a&b=3n, etc 
      * ??strat 1: calc a:b, figure out remainders (a/b)/(1/2)
      * filter2: if (a-b) > min(a,b) bc min not enough to compensate diff even with -1s 
-     * key values: (a - b), min(a,b) % 3,  
+     * key values: (a - b), min(a,b) % 2,  
     */
     
     return 0;
