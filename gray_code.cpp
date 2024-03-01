6377af2bcedc8b3c6312b0eda74a5f41f2213cd5
5handilya <kunalshandilyawork@gmail.com>
2024-03-01 06:18:48 +0530

    nothing real

---
+++ gray_code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> lines (2*n + 1, vector<int>(n, 0));
+    //sol1 didnt work, try new
+    return 0;
+}
